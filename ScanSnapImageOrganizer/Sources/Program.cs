0c3ea65c25ae189b02865f34da05a52d76ce1708
menyawa <41637976+menyawa@users.noreply.github.com>
2023-10-15 22:21:29 +0900

    各本の表紙の画像のパスのリストの取得までの、プログラムのメインの処理の流れを追加

--- ScanSnapImageOrganizer/Sources/Program.cs
+++ ScanSnapImageOrganizer/Sources/Program.cs
@@ -1,10 +1,34 @@
-﻿namespace ScanSnapImageOrganizer
+using System.Reflection;
+
+namespace ScanSnapImageOrganizer
 {
     internal class Program
     {
         private static void Main(string[] args)
         {
+#if DEBUG
+            var unitTester = new Test.UnitTester();
+            unitTester.TestAll();
+#endif
+
+            //画像ファイルのパスのリストを取得
+            //特に指定がない限り、フォルダ分けしたいフォルダに放り込む使い方を想定
+            var targetDirectoryPath = 0 < args.Length ? args[0] : "";
+            if (string.IsNullOrEmpty(targetDirectoryPath) || string.IsNullOrWhiteSpace(targetDirectoryPath))
+            {
+                targetDirectoryPath = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+            }
+            var allImageFilePaths = Directory.GetFiles(targetDirectoryPath, @"*.jpg").ToList();
             
+            //各本の表紙のリストを取得
+            var filter = new IO.FileNameFilter(@".*_[0-9]{3}\.jpg");
+            var titleFilePaths = filter.ListTitleFilePaths(allImageFilePaths);
+
+
+            Console.WriteLine("ScanSnapで生成した画像ファイルのフォルダ分けを開始します");
+            Console.WriteLine($"今回の実行時間：{DateTime.Now}");
+            Console.WriteLine($"フォルダ分け対象のディレクトリ：{targetDirectoryPath}");
+            Console.WriteLine("ScanSnapで生成した画像ファイルのフォルダ分けを完了しました");
         }
     }
 }
